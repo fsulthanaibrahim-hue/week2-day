4a77717a7c8a5a947899b05d92114f16fa966f59
fsulthanaibrahim-hue <fsulthanaibrahim@gmail.com>
2026-01-12 14:35:11 -0800

    week2-day

---
+++ Week2.js
@@ -0,0 +1,208 @@
+          //variables 
+           
+           
+//     //String
+// var person = "John";
+// let person1 = "Smith";
+// console.log(person)
+// console.log(typeof person)
+
+//     //Number
+// var number1 = 10;
+// var number2 = 20.675;
+// console.log(number2)
+// console.log(typeof number2)
+
+//   //Boolean
+// var js = true;
+// var html = false;
+// console.log(js)
+// console.log(typeof js)
+
+//     //undefined
+// var test1;
+// console.log(test1)
+// console.log(typeof test1)
+
+//   //Null
+// var test2 = null;
+// console.log(test2)
+// console.log(typeof test2)
+
+
+//eg:- 
+     
+//     string
+// var person = "John";
+// console.log(person);
+// person = "Anil";
+// console.log(person);
+
+//     //number
+// var a = 10, b = 20;
+// console.log(a + b)
+
+
+// const pi = 3.14;
+// console.log(pi)
+// pi = 3;
+// console.log(pi)
+
+   
+   
+              //Operators
+      
+   // Arithmetic Operators
+     
+//       //Add
+// var a = 10;
+// var b = 20;
+// console.log(a + b)
+    
+//         //sub
+// var a = 10;
+// var b = 20;
+// console.log(a - b)
+
+//       //multi
+// var a = 10;
+// var b = 20;
+// console.log(a * b)
+    
+//       //div
+// var a = 10;
+// var b = 20;
+// console.log(a / b)
+
+//       //modulus
+// var a = 10;
+// var b = 3;
+// console.log(a % b)
+
+
+//       //increment
+// var a = 10;
+// a ++; 
+// console.log(a)
+
+//     //exponentiation
+// var a = 20;
+// console.log(2 ** 3)
+
+
+                //Assignmnet Operators
+//         //Assign(=)
+// var x = 10;
+// console.log(x)
+
+//         //Add and Assign(+=)
+// var x = 10;
+// var result = x += 4;
+// console.log(result)
+       
+//         //Subtract and Assign(-=)
+// var x = 10;
+// var result = x -= 4;
+// console.log(result)
+    
+//         //Multiply and Assign(*=)
+// var x = 10;
+// x *= 4;
+// console.log(x)
+    
+//       //Divide and Assign(/=)
+// var x = 10;
+// var result = x /= 4;
+// console.log(result)
+
+
+    
+//       //Modulus and Assign(%=)
+// var x = 10;
+// var result = x %= 4;
+// console.log(result)
+
+
+                 // Comparison Operators
+//           //Equal to(==)
+// var a = 10;
+// var b = 20;
+// console.log(a == b)
+
+
+//           //Identical(===)
+// var a = 10;
+// var b = 10;
+// console.log(a === b)
+   
+//           //Greater than (>)
+// var a = 10;
+// var b = 20;
+// console.log(a > b)
+
+//             //Greater than or equal to(>=)
+            
+// var a = 10;
+// var b = 20;
+// console.log(a >= b)
+
+               // Logical Operators
+    
+//           //AND(&&)
+
+// var a = 10;
+// var b = 20;
+// console.log(a == 10  &&  b == 20)
+    
+//           //OR(||)
+
+// var a = 10;
+// var b = 20;
+// console.log(a == 10  ||  b == 20)    
+    
+//         //NOT(!)
+         
+// var a = 10;
+// var b = 20;
+// console.log(!(b == 20))
+         
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
